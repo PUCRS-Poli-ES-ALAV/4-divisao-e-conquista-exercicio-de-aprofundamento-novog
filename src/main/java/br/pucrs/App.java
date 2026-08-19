8c3700cfea522f06a2ddead96008f4e6b2b2aec1
Eduardo Dieter Wächter <eduardo.d.wachter@gmail.com>
2026-08-19 23:39:13 +0000

    fizezmos quase nada

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,5 +1,7 @@
 package br.pucrs;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public ArrayList<> mergeSort(ArrayList<Int> lista){
+        if(lista.size()==1){
+            return lista;
+        }
+        else{
+            ArrayList a = mergeSort(lista.subList(0,lista.size()/2));
+            ArrayList b = mergeSort(lista.subList(lista.size()/2,lista.size()));
+        }
+    }
+    public ArrayList<Int> organiza(ArrayList<Int> a, ArrayList<Int> b){
+//novo ArrayList
+ArrayList ordenada = new ArrayList<Int> ();
+
+        for (int i =0;i<a.size();i++) {
+            
+        }
     }
 }
