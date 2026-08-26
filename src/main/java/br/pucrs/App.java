3a1a27ff5ad96518db9f5d7798e78bafc61236ae
Eduardo Dieter Wächter <eduardo.d.wachter@gmail.com>
2026-08-26 22:57:05 +0000

    implementacao de todos os metodos

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,7 +1,20 @@
 package br.pucrs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        //1
+        ArrayList<Integer> lista = new ArrayList<>(List.of(1, 10, 23, 4, 2, 5, 3));
+        System.out.println(mergeSort(lista));
+        //2
+        long[] A = {1, 10, 23, 4, 2, 5, 3};
+        System.out.println(maxVal1(A, A.length));
+        //3
+        System.out.println(maxVal2(A, 0, A.length - 1));
+
+        //aqui 4
         System.out.println(MULTIPLY(7, 5, 4));
         System.out.println(MULTIPLY(12345, 6789, 16));
         System.out.println(MULTIPLY(123456789L, 987654321L, 64));
@@ -28,6 +41,76 @@ public class App {
         }
     }
 
+    //mergesort
+    public static ArrayList<Integer> mergeSort(ArrayList <Integer> lista){
+        int tam = lista.size();
+        if(tam < 2){
+            return lista;
+        }
+       ArrayList<Integer> listdown = new ArrayList<> (lista.subList(tam/2,tam));
+       ArrayList<Integer> listup = new ArrayList<> (lista.subList(0, tam/2));
+
+       //mergeSort(listadown);
+       //mergeSort(listaup);
+        mergeSort(listdown);
+        mergeSort(listup);
+        return merge(lista, listdown, listup);
+
+    }
+
+    public static ArrayList<Integer> merge(ArrayList <Integer> lista, ArrayList <Integer> listdown, ArrayList <Integer> listup){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < listdown.size() && j < listup.size()){
+            if(listdown.get(i) <= listup.get(j)){
+                lista.set(k, listdown.get(i));
+                i++;
+            }else{
+                lista.set(k, listup.get(j));
+                j++;
+            }
+            k++;
+        }
+        while(i < listdown.size()){
+            lista.set(k, listdown.get(i));
+            i++;
+            k++;
+        }
+        while(j < listup.size()){
+            lista.set(k, listup.get(j));
+            j++;
+            k++;
+        }
+        return lista;
+    }
+
+    //2
+    public static long maxVal1(long A[], int n) {  
+       long max = A[0];
+       for (int i = 1; i < n; i++) {  
+           if( A[i] > max ) 
+              max = A[i];
+       }
+       return max;
+   }
+   //3
+   public static long max(long a, long b) {  
+       if (a > b) 
+           return a;  
+       else 
+           return b;  
+   }
+   public static long maxVal2(long A[], int init, int end) {  
+       if (end - init <= 1)
+           return max(A[init], A[end]);  
+       else {
+             int m = (init + end)/2;
+             long v1 = maxVal2(A,init,m);   
+             long v2 = maxVal2(A,m+1,end);  
+             return max(v1,v2);
+            }
+   }
     // Multiplica duas strings de bits x e y
     // e retorna o resultado como um inteiro "long".
     // transforma bits em long, chama MULTIPLY e retorna o resultado
@@ -40,11 +123,5 @@ public class App {
     //ainda nao implementado
     //ideia para contar as iteracoes 
     //realizadas apartir de cada entrada 
-    public long getInterations(){
-        return interationCount;
-    }
 
-    public void resetInterationCount(){
-        interationCount = 0;
-    }
 }
